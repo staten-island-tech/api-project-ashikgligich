2850f258e230f22dd2f8611980c89c0fe729b7fd
ashikgligich <ashton.gligich@gmail.com>
2023-12-13 09:16:46 -0500

    e
    e

--- project/js/uhh.js
+++ project/js/uhh.js
@@ -1,39 +1,55 @@
 function greet(name) {
-    const greetPromise = new Promise(function (resolve, reject) {
-        return('hello ${name}');
-    });
-    return greetPromise
+  const greetPromise = new Promise(function (resolve, reject) {
+    return "hello ${name}";
+  });
+  return greetPromise;
+}
+
+const Ash = greet("ash");
+console.log(Ash);
+Ash.then((result) => {
+  console.log(result);
+});
+
+// api key naza = 5LF5j2deuntZyGeVIE7oLmoS1yIuhrNnrzkRRRPa
+const URL = `https://api.nasa.gov/planetary/apod?api_key=5LF5j2deuntZyGeVIE7oLmoS1yIuhrNnrzkRRRPa`;
+
+async function getData(URL) {
+  try {
+    const responce = await fetch(URL);
+    if (responce.status != 200) {
+      throw new Error(responce.statusText);
     }
-    
-    const Ash = greet("ash");
-    console.log(Ash);
-    Ash.then((result)=>{
-        console.log(result);
-    });
-    
-    // api key naza = 5LF5j2deuntZyGeVIE7oLmoS1yIuhrNnrzkRRRPa
-    const URL = `https://api.nasa.gov/planetary/apod?api_key=5LF5j2deuntZyGeVIE7oLmoS1yIuhrNnrzkRRRPa`;
-    
-    async function getData(URL){
-        try{
-            const responce = await fetch(URL);
-            if(responce.status != 200){
-                throw new Error(responce.statusText);
-            }
-            console.log(responce);
-            const data = await responce.json();
-            console.log(data);
-            console.log(data.url);
-            document.body.style.backgroundImage = `url(${data.url})`;
-        } catch(error){
-            console.error("Error fetching data:", error);
-            document.querySelector("h1").textContent = "Error fetching NASA data";
-        }
-       // fireship
+    console.log(responce);
+    const data = await responce.json();
+    console.log(data);
+    console.log(data.url);
+    document.body.style.backgroundImage = `url(${data.url})`;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    document.querySelector("h1").textContent = "Error fetching NASA data";
+  }
+  // fireship
+}
+getData(URL);
+
+// api key naza = 5LF5j2deuntZyGeVIE7oLmoS1yIuhrNnrzkRRRPa
+
+const URL2 = `https://techport.nasa.gov/api/projects?updatedSince=2023-11-01`;
+
+async function getData(URL2) {
+  try {
+    const responce = await fetch(URL);
+    if (responce.status != 200) {
+      throw new Error(responce.statusText);
     }
-    getData(URL);
-
-
-    
-
-
+    console.log(responce);
+    const data = await responce.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    document.querySelector("h1").textContent = "Error fetching NASA data";
+  }
+  // fireship
+}
+getData(URL);
